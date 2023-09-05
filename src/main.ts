75c6964f9ad3397803acd0904e7a91fc0153aaff
A-Annan <annanabdelilah@gmail.com>
2023-09-05 01:38:14 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      /.*/,
+      // manager_url,
+      // teacher_url,
+      // student_url,
+      // teacher_ui_url_prod,
+      // manager_ui_url_prod,
+      // student_ui_url_prod,
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
