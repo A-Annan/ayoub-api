cb71967ffe6f0e2d061f4a1eb364251c1887d569
ayoub1445 <ayoubannan16@gmail.com>
2023-09-05 23:24:12 +0100

    me

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      /.*/,
-      // manager_url,
-      // teacher_url,
-      // student_url,
-      // teacher_ui_url_prod,
-      // manager_ui_url_prod,
-      // student_ui_url_prod,
-    ],
-  });
+  app.enableCors(
+    
+  )
   await app.listen(3000);
 }
 bootstrap();
