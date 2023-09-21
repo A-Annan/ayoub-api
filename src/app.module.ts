1db7bc8d5f561bc59edf72f0cdb31247e27d116c
A-Annan <annanabdelilah@gmail.com>
2023-09-21 23:24:11 +0100

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,20 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsModule } from './projects/projects.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://ayoubannan16:EtvklQsNcP5StKYA@database.fixqxhq.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('db'),
+        };
+      },
+      inject: [ConfigService],
+    }),
     UsersModule,
     ProjectsModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('process.env.db', process.env.db);
+
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
